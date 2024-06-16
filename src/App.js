e0e3dadd8c1d57b06856415df273fa43580ce31a
kushal1997 <kushalrao103@gmail.com>
2024-06-16 07:35:54 +0530

    addded aipi to handle project section

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
+require("dotenv").config();
 
 function App() {
+  // console.log("env",process.env.REACT_APP_BACKEND_URL)
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
